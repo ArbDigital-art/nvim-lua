b2fcb5923213d91217a59fd8f50cfab578cc9c35
arbdigital-art <arb.digitalart@gmail.com>
2024-11-27 23:55:38 -0300

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,10 @@
 local map = vim.keymap.set
 
 
+map('n', '<C-\\>',     '<cmd>NvimTreeToggle<cr>',    { desc = 'NvimTreeToggle' })
+map('n', '<leader>cb', '<cmd>:bd!<cr>',              { desc = 'Close Current Buffer' })
+map('',  '<leader>rr', ':source %<cr>',              { desc = 'Source the current File' })
+map('n',  '<C-n>',     ':bnext<cr>',                 { desc = 'Go to Next Buffer' })
 
-map('n', '<C-\\>', '<cmd>NvimTreeToggle<cr>')
+map('n',  '<leader>ff', ':Telescope find_files<cr>', { desc = 'Telescope Find Files' })
+map('n',  '<leader>ob', ':Telescope buffers<cr>',    { desc = 'Telescope: Show Opened Buffer' })
